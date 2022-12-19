e0f5662add90861324e28a9444b2ebed843a1f81
Michael Bonyo <bonyomichael406@gmail.com>
2022-12-19 01:31:24 -0800

    correction Task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     """prints x elements of a list"""
     n = 0
-    try:
-        for index in range(0, x):
+    for index in range(0, x):
+        try:
             print(my_list[index], end="")
             n += 1
-        print()
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-        raise
-    return n
+        except IndexError:
+            break
+    print()
+    return (n)
