55d494993e50a5fb85b35bf349750857f4b0680e
Michael Bonyo <bonyomichael406@gmail.com>
2022-12-20 04:21:54 -0800

    define class square where sie is a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 0-square.py
+# Michael O Bonyo
+"""Define a class square"""
+
+
+class Square:
+    """Create a square"""
+
+    def __init__(self, size):
+        """Initializing size value
+
+        Args:
+            size (int): Size of the created square
+        """
+        self.__size = size
