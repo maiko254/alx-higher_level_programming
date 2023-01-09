53d714dbd5c32a21970a6815109ab8529daafa30
Michael Bonyo <bonyomichael406@gmail.com>
2023-01-09 10:11:35 -0800

    implemented class rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,12 +13,12 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
         """validates value
-        
+
         Args:
             name (string): name
             value (int): value to validate
         """
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
