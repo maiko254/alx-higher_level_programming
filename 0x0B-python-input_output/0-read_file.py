257489aa5993915f49cd1e5f2b2c5d6d92708825
Michael Bonyo <bonyomichael406@gmail.com>
2023-01-10 04:38:38 -0800

    read utf-8 encoded text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-read_file.py
+# Michael O Bonyo
+"""Implements a file reader"""
+
+
+def read_file(filename=""):
+    """defines how to read a UTF8 encoded text file
+
+    Args:
+        filename (string): name of file to read from
+    """
+    with open(filename, encoding='utf-8') as a_file:
+        print(a_file.read())
