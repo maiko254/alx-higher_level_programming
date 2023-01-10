a433ffcc02114dde08021645a29fa2c515d40671
Michael Bonyo <bonyomichael406@gmail.com>
2023-01-10 04:54:21 -0800

    correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,10 +5,10 @@
 
 
 def read_file(filename=""):
-    """defines how to read a UTF8 encoded text file
+    """defines how to read a UTF8 encoded text file and print to stdout
 
     Args:
         filename (string): name of file to read from
     """
-    with open(filename, encoding='utf-8') as a_file:
+    with open(filename, encoding="utf-8") as a_file:
         print(a_file.read(), end="")
