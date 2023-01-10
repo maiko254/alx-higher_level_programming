969bd42ac80c9582cc7d6fd6e0483e3e97295030
Michael Bonyo <bonyomichael406@gmail.com>
2023-01-10 09:24:21 -0800

    correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-items = []
-items = load_from_json_file("add_item.json")
-items.extend(sys.argv[1:])
-save_to_json_file(items, "add_item.json")
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    items = []
+    items = load_from_json_file("add_item.json")
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
