370ee292fe1659df9b0e779c921e743787d09422
Michael Bonyo <bonyomichael406@gmail.com>
2023-01-11 04:35:02 -0800

    added public method that replaces all attributes of student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# 10-student.py
+# Michael O Bonyo
+"""Implements class Student"""
+
+
+class Student:
+    """Defines a an object student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of a student object
+
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of student
+            age (int): age of the student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): list containing the attributes to represent
+        """
+        if type(attrs) == list:
+            for items in attrs:
+                if type(items) == str:
+                    for i in attrs:
+                        if hasattr(self, i):
+                            return {i: getattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all the attributes of the student instance
+
+        Args:
+            json (dict): dictionary containing the attributes
+        """
+        for j, k in json.items():
+            setattr(self, j, k)
