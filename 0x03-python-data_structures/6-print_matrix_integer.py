a9a8b92f4ffd7181518d2d6141e891ac9607fffe
maiko254 <bonyomichael406@gmail.com>
2023-12-04 11:06:25 +0000

    fixed print using str.format()

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
-            print(i[j], end="")
+            print("{}".format(i[j]), end="")
             if j != (len(i) - 1):
                 print(" ", end="")
         print("")
