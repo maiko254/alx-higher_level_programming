187c2f3181f21782a02c36ee4a5c5c080b401759
maiko254 <bonyomichael406.com>
2024-01-06 14:01:21 +0300

    correction

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -68,4 +68,5 @@ class Rectangle:
 
     def __repr__(self):
         """Returns the printable representation that can recreate Rectangle"""
-        return("Rectangle({:d}, {:d})".format(self.__width, self.__height))
+
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
