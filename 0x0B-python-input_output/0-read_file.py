12f6af78ed7fcf37c3d337a61aec390a2ab90901
maiko254 <bonyomichael406.com>
2024-01-09 12:27:54 +0300

    read file: correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,4 @@
 def read_file(filename=""):
     """ reads a textfile """
     with open(filename, encoding="utf-8") as f:
-        data = f.read()
-        print(data)
+        print(f.read(), end='')
