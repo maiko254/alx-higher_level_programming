b64542cfc67afaccf17891294844f3abbb4e2930
maiko254 <bonyomichael406@gmail.com>
2024-02-13 19:56:05 +0300

    Introduction to SQL

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- script to updated score value in second table
+UPDATE second_table SET score = 10 WHERE name = "Bob";
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- deletes a database if it exists in MySQL server
+DROP DATABASE IF EXISTS hbtn_0c_0
