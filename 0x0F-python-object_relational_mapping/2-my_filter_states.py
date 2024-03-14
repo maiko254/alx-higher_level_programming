f267e07650626504428b9b425c9558ba063423e8
maiko254 <bonyomichael406.com>
2024-03-14 20:46:12 +0300

    search table records using value passed in as argument to script

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" script to search database table for records of name passed as argument """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    host = "localhost"
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    port = 3306
+    name = argv[4]
+
+    try:
+        conn = MySQLdb.connect(host, user, passwd, db, port)
+        cur = conn.cursor()
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(name)
+        cur.execute(query)
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        print(f"MySQL Error {str(e):s}")
+
+    for row in rows:
+        print(f"{row}")
+
+    cur.close()
+    conn.close()
