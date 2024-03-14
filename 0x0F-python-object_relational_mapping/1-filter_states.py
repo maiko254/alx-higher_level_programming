32d70152534f5b9d72db121ac0aa622819d592ed
maiko254 <bonyomichael406.com>
2024-03-14 20:00:54 +0300

    Select names from table states starting with letter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" selects from table states all states whose name starts with N """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    host = "localhost"
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    port = 3306
+
+    try:
+        conn = MySQLdb.connect(host, user, passwd, db, port)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        print(f"MySQL Error {str(e):s}")
+
+    for row in rows:
+        print(f"{row}")
