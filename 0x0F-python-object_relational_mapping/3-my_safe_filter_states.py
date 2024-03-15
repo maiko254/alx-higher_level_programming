415d27456c4cc8b9fbe6ce2d19540ace653db1df
maiko254 <bonyomichael406.com>
2024-03-15 11:22:22 +0300

    script to search table using value passed in as argument and is safe from sql injection attack

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" 
+   script to search database table for records of name passed as argument
+   that is safe from an sql injection attack
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    host = "localhost"
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    port = 3306
+    name = argv[4]
+
+    try:
+        conn = MySQLdb.connect(host, user, passwd, db, port)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (name,))
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        print(f"MySQL Error {str(e):s}")
+
+    for row in rows:
+        print(f"{row}")
+
+    cur.close()
+    conn.close()
