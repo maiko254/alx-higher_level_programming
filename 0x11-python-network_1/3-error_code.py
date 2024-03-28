4d6f5a3188662c6dfc83ffdc3a45f9f4efeada44
maiko254 <bonyomichael406.com>
2024-03-28 15:21:47 +0300

    script that fetches a URL and also handles the HTTPError exception

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Script that takes in and fetches a URL and handles HTTPError exception """
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
