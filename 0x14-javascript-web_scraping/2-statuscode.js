90448d397678c97f0a3e711d4a15a292ab31f550
maiko254 <bonyomichael406.com>
2024-04-23 15:25:10 +0300

    Correction

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,7 @@ const reqURL = process.argv[2];
 request(reqURL, (error, response, body) => {
   if (error) {
     console.error(error);
+    return;
   }
   console.log('code: ', response.statusCode);
 });
